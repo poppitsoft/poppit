22cbf8419e2ef7c089972b3a6ba8e00618200b38
Muhammad Amer <poppitsoft@gmail.com>
2026-07-09 13:32:51 +0500

    Add custom build script

---
+++ scripts/build.mjs
@@ -0,0 +1,28 @@
+import { spawnSync } from "node:child_process";
+import { join } from "node:path";
+
+const isWindows = process.platform === "win32";
+
+const tsc = isWindows
+  ? join("node_modules", ".bin", "tsc.cmd")
+  : join(".", "node_modules", ".bin", "tsc");
+
+console.log("==================================");
+console.log("Poppit Build Started");
+console.log("==================================");
+
+const result = spawnSync(
+  tsc,
+  ["-p", "apps/server/tsconfig.json"],
+  {
+    stdio: "inherit",
+  }
+);
+
+if (result.status !== 0) {
+  process.exit(result.status);
+}
+
+console.log("==================================");
+console.log("Poppit Build Finished");
+console.log("==================================");
